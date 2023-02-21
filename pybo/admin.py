7e98455ee46c94ac4148a515d1a7bdc296ff6e22
JaeHwan Kim <dksrudwpal@gmail.com>
2023-02-21 10:10:43 +0900

    설정

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Answer
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+    list_display = ['id', 'subject', 'create_date']
+    ordering = ['-id'] # 오름차순 정렬. 앞에 -를 붙이면 내림차순 정렬이 된다. 
+    
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
+
--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    subject = models.CharField('제목', max_length=200)
+    content = models.TextField('내용')
+    create_date = models.DateTimeField('생성일')
+
+    def __str__(self):
+        return f'{self.id} {self.subject}'
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.content
