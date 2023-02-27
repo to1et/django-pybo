56fb2b2d9154129d598fab9ee4cd59c12e6305aa
JaeHwan Kim <dksrudwpal@gmail.com>
2023-02-27 10:41:42 +0900

    질문 수정 삭제까지

--- pybo/urls.py
+++ pybo/urls.py
@@ -6,8 +6,14 @@ app_name = 'pybo'
 urlpatterns = [
     path('', views.index, name='index'),                      # 목록 보기
     path('<int:question_id>/', views.detail, name='detail'),   # 상세 보기
+
+    
     path('answer/create/<int:question_id>/', views.answer_create,
          name='answer_create'),
+
     path('question/create/', views.question_create, name='question_create'),
+    path('question/modify/<int:question_id>/', views.question_modify,
+          name='question_modify'),
+    path('question/delete/<int:question_id>/', views.question_delete, name='question_delete'), 
 
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -3,6 +3,8 @@ from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -28,6 +30,57 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question':question}
     return render(request, 'pybo/question_detail.html', context)
+
+@login_required(login_url='common:login')
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    if request.method == 'POST': # submit을 통한 POST 요청, 사용자가 입력한 데이터가 들어감
+        form = QuestionForm(request.POST)
+        if form.is_valid(): # 유효성 검사가 통과하면 아래 실행. (models.py에서 subject, content가 요구됨. 모두 입력되면 유효성검사 통과)
+            question = form.save(commit=False)
+            question.author = request.user
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:                        # GET 요청
+        form = QuestionForm() #비어있는 데이터가 들어간다.
+
+    context = {'form': form} #is_valid()가 false가 되면 이쪽으로 바로 넘어옴.
+                             #POST 일때는 if아래의 form이, GET 일때는 else 아래의 내용이 비어있는 form이 넘어옴
+                             #사용자가 잘못 입력한 값이 있을 경우 if아래의 form이 그것을 기억해 두었다가 반환해줌.
+    return render(request, 'pybo/question_form.html', {'form': form})
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """
+    pybo 질문수정
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question.id)
+    
+    if request.method == "POST":
+        #질문 수정을 위해 값 덮어쓰기
+        form = QuestionForm(request.POST, instance=question)
+        #instance=question: 원본데이터값 가져옴 request.POST 는 유저가 요청한 값. 원본데이터값에 덮어씌운다.
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        #질문 수정 화면에 기존 제목, 내용 반영
+        form = QuestionForm(instance=question)
+
+    context = {'form': form}
+
+    return render(request, 'pybo/question_form.html', context)
+
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """
     pybo 답변등록
@@ -45,6 +98,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question #FK 직접 처리
             answer.save()
@@ -60,21 +114,17 @@ def answer_create(request, question_id):
     #                            create_date=timezone.now())
     # return redirect('pybo:detail', question_id=question.id)
 
-def question_create(request):
-    """
-    pybo 질문등록
-    """
-    if request.method == 'POST': # submit을 통한 POST 요청, 사용자가 입력한 데이터가 들어감
-        form = QuestionForm(request.POST)
-        if form.is_valid(): # 유효성 검사가 통과하면 아래 실행. (models.py에서 subject, content가 요구됨. 모두 입력되면 유효성검사 통과)
-            question = form.save(commit=False)
-            question.create_date = timezone.now()
-            question.save()
-            return redirect('pybo:index')
-    else:                        # GET 요청
-        form = QuestionForm() #비어있는 데이터가 들어간다.
 
-    context = {'form': form} #is_valid()가 false가 되면 이쪽으로 바로 넘어옴.
-                             #POST 일때는 if아래의 form이, GET 일때는 else 아래의 내용이 비어있는 form이 넘어옴
-                             #사용자가 잘못 입력한 값이 있을 경우 if아래의 form이 그것을 기억해 두었다가 반환해줌.
-    return render(request, 'pybo/question_form.html', {'form': form})
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    """
+    pybo 질문삭제
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제 권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question.id)
+    question.delete()
+    return redirect('pybo:index')
+
+
