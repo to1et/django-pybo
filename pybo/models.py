911bb382ff2ec82a9c80a91a52498a6c7d7a36aa
JaeHwan Kim <dksrudwpal@gmail.com>
2023-02-28 16:27:59 +0900

    vote 기능 완성

--- pybo/models.py
+++ pybo/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='author_question')
+                                #이름 변경 voter의 User와 겹치기 때문에 이를 방지하기 위해 이름을 변경해준다.
     subject = models.CharField('제목', max_length=200,
                                 help_text='질문의 제목을 한 줄로 작성하세요.')
     content = models.TextField('내용', help_text='질문의 내용을 상세히 작성하세요.')
@@ -17,12 +18,14 @@ class Question(models.Model):
         return f'{self.id} {self.subject}'
 
 class Answer(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='author_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField('답변 내용')
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
 
+    voter = models.ManyToManyField(User, related_name='voter_answer')
     def __str__(self):
         return self.content
     
