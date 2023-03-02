d961743958b8c35d37da53e3a8e9c7499712215c
JaeHwan Kim <dksrudwpal@gmail.com>
2023-03-02 16:47:05 +0900

    마크다운, 검색, 정렬 추가

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -8,3 +8,8 @@ register = template.Library()
 def mark(value):
     extensions = ["nl2br", "fenced_code"]
     return mark_safe(markdown.markdown(value, extensions=extensions))
+
+@register.simple_tag()
+def avatar(uid):
+    tag = f'<img class="avatar" src="https://randomuser.me/api/portraits/men/{uid}.jpg"/>'
+    return mark_safe(tag)
