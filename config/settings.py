0c59b0fb7ded8091f2fe4372d6c123cbe6a30e77
JaeHwan Kim <dksrudwpal@gmail.com>
2023-03-03 13:55:21 +0900

    heidi SQL 연동

--- config/settings.py
+++ config/settings.py
@@ -78,9 +78,18 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # 
+    # }
+    'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'HOST': 'localhost',
+    'PORT': '3306',
+    'NAME': 'pybo_db',
+    'USER': 'iot',
+    'PASSWORD': '1234'
     }
 }
 
